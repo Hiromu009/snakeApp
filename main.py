2a34dca713abb2b41e8b0b66c0a543125b12e2bf
Hiromu009 <hiromut99@gmail.com>
2024-03-07 03:12:24 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from mainmenu import main_menu
+from scoreboard import display_top_scores_gui
+
+if __name__ == "__main__":
+    from mainmenu import main_menu
+    while True:
+        main_menu()
+        display_top_scores_gui()
